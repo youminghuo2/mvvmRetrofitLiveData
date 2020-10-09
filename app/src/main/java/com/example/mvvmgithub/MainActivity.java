f55a37295a6ec6bd2533f1036aa5e4fc123bef4a
Zhu ChenYi <zcy@unisolution.cn>
2020-10-09 16:02:02 +0800

    更新成mvvm框架

--- app/src/main/java/com/example/mvvmgithub/MainActivity.java
+++ app/src/main/java/com/example/mvvmgithub/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.mvvmgithub;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.example.mvvmgithub.api.Api;
-import com.example.mvvmgithub.api.ApiService;
 import com.example.mvvmgithub.databinding.ActivityMainBinding;
 import com.example.mvvmgithub.model.GetJoke;
+import com.example.mvvmgithub.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        ApiService.create("https://api.apiopen.top/", Api.class).getJokeText().observe(this, new Observer<GetJoke>() {
+
+        final MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.getJokeLiveData().observe(this, new Observer<GetJoke>() {
             @Override
             public void onChanged(GetJoke getJoke) {
-                Log.d("--", getJoke.toString());
-                activityMainBinding.text.setText(getJoke.getResult().get(0).getText());
-                activityMainBinding.name.setText(getJoke.getResult().get(0).getName());
+                if (getJoke != null) {
+                    activityMainBinding.setJoke(getJoke.getResult().get(0));
+                }
             }
         });
     }
--- app/src/main/java/com/example/mvvmgithub/api/Api.java
+++ app/src/main/java/com/example/mvvmgithub/api/Api.java
@@ -8,13 +8,13 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Api {
-    //    @GET("getJoke")
-//    LiveData<GetJoke> getJokeText(
-//            @Query("page") int page,
-//            @Query("count") int count,
-//            @Query("type") String type
-//    );
-    @GET("getJoke?page=1&count=2&type=video")
+    @GET("getJoke")
     LiveData<GetJoke> getJokeText(
+            @Query("page") int page,
+            @Query("count") int count,
+            @Query("type") String type
     );
+//    @GET("getJoke?page=1&count=2&type=video")
+//    LiveData<GetJoke> getJokeText(
+//    );
 }
